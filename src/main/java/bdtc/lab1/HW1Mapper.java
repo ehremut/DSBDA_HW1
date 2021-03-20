ad8fbf22d05f13bd4cd495471106403de02e7d23
ehremut <34132244+ehremut@users.noreply.github.com>
2021-03-20 16:39:51 +0300

    Update HW1Mapper.java
    Add comments

--- src/main/java/bdtc/lab1/HW1Mapper.java
+++ src/main/java/bdtc/lab1/HW1Mapper.java
@@ -15,28 +15,22 @@ import java.util.regex.*;
 public class HW1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(); // variable value initialization
     private Text word = new Text(); // key initialization
-    private static String regexStr = "^20[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3])(:(0[0-9]|[1-5][0-9])){2},[0-7]$";
-    Pattern pattern = Pattern.compile(regexStr);
+    private static String regexStr = "^20[0-9]{2}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1]) (0[0-9]|1[0-9]|2[0-3])(:(0[0-9]|[1-5][0-9])){2},[0-7]$"; //регулярное выражение
+    Pattern pattern = Pattern.compile(regexStr); // компилляция регулярного выражения
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        Matcher matcher = pattern.matcher(line);
-        if (!matcher.find()) {
-            // increment counter if error
-            context.getCounter(CounterType.MALFORMED).increment(1);
+        String line = value.toString();  // перевод строки в string
+        Matcher matcher = pattern.matcher(line); //поиск совпадений
+        if (!matcher.find()) { // условие если не найдено
+            context.getCounter(CounterType.MALFORMED).increment(1); // инкрементим счетчик при ошибке
 
         } else {
-            // get elements of good line with split
-            String[] stringElements = line.split("[:,]");
-            // check for empty
-            if (Arrays.stream(stringElements).findFirst().isPresent() & !stringElements[stringElements.length - 1].isEmpty()) {
-                // set key
-                word.set(Arrays.stream(stringElements).findFirst().orElse(""));
-                // set value
-                one.set(Integer.parseInt(stringElements[stringElements.length - 1]));
-                // write to context
-                context.write(word, one);
+            String[] stringElements = line.split("[:,]"); //разбиваем строку на ключ и значение
+            if (Arrays.stream(stringElements).findFirst().isPresent() & !stringElements[stringElements.length - 1].isEmpty()) { // проверка, что полученные ключ и значение не пустые
+                word.set(Arrays.stream(stringElements).findFirst().orElse("")); // сеттим ключ
+                one.set(Integer.parseInt(stringElements[stringElements.length - 1])); // сеттим значение
+                context.write(word, one); // записываем в контекст
             }
         }
     }
