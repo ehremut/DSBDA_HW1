6dac25f8d83b459838e746ca7852a605140f4bd7
ehremut <34132244+ehremut@users.noreply.github.com>
2021-03-20 16:45:59 +0300

    Update HW1Reducer.java
    add comments

--- src/main/java/bdtc/lab1/HW1Reducer.java
+++ src/main/java/bdtc/lab1/HW1Reducer.java
@@ -8,32 +8,32 @@ import java.util.*;
 
 public class HW1Reducer extends Reducer<Text, IntWritable, Text, Text> {
     private static final String[] values_key =
-            {"emerg", "alert", "crit", "err", "warning", "notice", "info", "debug"}; // array of keys
+            {"emerg", "alert", "crit", "err", "warning", "notice", "info", "debug"}; // список названий значений
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        String keyDict = "";
-        Map<String, Integer> dict = new HashMap<String, Integer>();
-        while (values.iterator().hasNext()) {
-            int element = values.iterator().next().get();
-            int valueDict = 0;
-            keyDict = values_key[element];
-            if (dict.get(keyDict) != null) {
-                valueDict = dict.get(keyDict) + 1;
-                dict.put(keyDict, valueDict);
+        String keyDict = ""; // ключ
+        Map<String, Integer> dict = new HashMap<String, Integer>(); // словарь для названия предупреждения и его количества
+        while (values.iterator().hasNext()) { // итерация по элементам
+            int element = values.iterator().next().get(); // записываем элемент
+            int valueDict = 0; //инициализтируем переменную, которая будет служить значением для словаря
+            keyDict = values_key[element]; // достаем ключ по индексу из массива предупреждений
+            if (dict.get(keyDict) != null) { // проверка, что такое значение уже есть в словаре
+                valueDict = dict.get(keyDict) + 1; // увеличиваем значение по этому ключу
+                dict.put(keyDict, valueDict); // записываем в словарь
             }
             else {
-                dict.put(keyDict, 1);
+                dict.put(keyDict, 1); // если встретилось впервые, то ставим значение 1
             }
         }
 
-        String dictString = "";
-        Enumeration<String> strEnum = Collections.enumeration(dict.keySet());
+        String dictString = ""; // инициализируем результирующую строку
+        Enumeration<String> strEnum = Collections.enumeration(dict.keySet()); // делаем итератор для хождения по ключам словаря
         while(strEnum.hasMoreElements()) {
-            String localKey = strEnum.nextElement();
-            dictString = dictString + " " + localKey + " : " + dict.get(localKey) + "; ";
+            String localKey = strEnum.nextElement(); // сохраняем ключ локально
+            dictString = dictString + " " + localKey + " : " + dict.get(localKey) + "; "; // пишем в результирующую строку
         }
-        Text value = new Text(dictString);
-        context.write(key, value);
+        Text value = new Text(dictString); // переводим в Text()
+        context.write(key, value); // записываем в context
     }
 }
